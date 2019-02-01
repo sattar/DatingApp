c328315bc14cc755fd7aea685df26e2fc22d989d
areen sattar <asattar@mathematica-mpr.com>
2019-02-01 14:14:36 -0800

    end of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace Datingpp.API.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
